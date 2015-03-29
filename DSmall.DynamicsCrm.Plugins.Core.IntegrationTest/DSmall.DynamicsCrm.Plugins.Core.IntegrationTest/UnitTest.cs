27c9c2feebf65ac92ee82eb2249c3754eab8397d
Davesmall28 <dave@dsmall.co.uk>
2015-03-29 22:06:02 +0100

    Add Integration test solution and project.

---
+++ DSmall.DynamicsCrm.Plugins.Core.IntegrationTest/DSmall.DynamicsCrm.Plugins.Core.IntegrationTest/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿namespace DSmall.DynamicsCrm.Plugins.Core.IntegrationTest
+{
+    using NUnit.Framework;
+
+    /// <summary>The unit test.</summary>
+    [TestFixture]
+    public class UnitTest
+    {
+        /// <summary>The test method.</summary>
+        [Test]
+        public void TestMethod()
+        {
+        }
+    }
+}
