b7adfc113143567e51b2a50b323c9f41e755a16d
Davesmall28 <dave@dsmall.co.uk>
2015-03-30 22:44:26 +0100

    Initial wireframe integration tests.

--- DSmall.DynamicsCrm.Plugins.Core.IntegrationTest/DSmall.DynamicsCrm.Plugins.Core.IntegrationTest/UnitTest.cs
+++ DSmall.DynamicsCrm.Plugins.Core.IntegrationTest/DSmall.DynamicsCrm.Plugins.Core.IntegrationTest/UnitTest.cs
@@ -1,10 +1,11 @@
 ﻿namespace DSmall.DynamicsCrm.Plugins.Core.IntegrationTest
 {
+    using DSmall.UnitTest.Core;
     using NUnit.Framework;
 
     /// <summary>The unit test.</summary>
     [TestFixture]
-    public class UnitTest
+    public class UnitTest : CrmIntegrationSpecificationBase
     {
         /// <summary>The test method.</summary>
         [Test]
