b6b0eb82863dd8ba8724fb4dfbd68132db7c7679
Davesmall28 <dave@dsmall.co.uk>
2015-04-01 22:22:07 +0100

    Integration test wireframe.

---
+++ DSmall.DynamicsCrm.Plugins.Core.IntegrationTest/DSmall.DynamicsCrm.Plugins.Core.IntegrationTest/Create/CreateContactSpecification.cs
@@ -0,0 +1,45 @@
+﻿namespace DSmall.DynamicsCrm.Plugins.Core.IntegrationTest
+{
+    using System;
+    using DSmall.UnitTest.Core;
+    using NUnit.Framework;
+
+    /// <summary>The create contact specification.</summary>
+    [TestFixture]
+    public class CreateContactSpecification : SpecificationBase
+    {
+        private CreateContactSpecificationFixture testFixture;
+
+        private Guid result;
+
+        /// <summary>The should return a non empty id.</summary>
+        [Test]
+        public void ShouldReturnANonEmptyId()
+        {
+            Assert.AreNotEqual(Guid.Empty, result);
+        }
+
+        /// <summary>The because of.</summary>
+        protected override void BecauseOf()
+        {
+            base.BecauseOf();
+
+            result = testFixture.CrmHelper.CreateContact(testFixture.EntityToCreate);
+        }
+
+        /// <summary>The context.</summary>
+        protected override void Context()
+        {
+            base.Context();
+
+            testFixture = new CreateContactSpecificationFixture();
+            testFixture.PerformTestSetup();
+        }
+
+        /// <summary>The clean up.</summary>
+        protected override void Cleanup()
+        {
+            testFixture.CleanUp.DeleteEntity("contact", result);
+        }
+    }
+}
---
+++ DSmall.DynamicsCrm.Plugins.Core.IntegrationTest/DSmall.DynamicsCrm.Plugins.Core.IntegrationTest/Utilities/CrmCleaner.cs
@@ -0,0 +1,26 @@
+﻿namespace DSmall.DynamicsCrm.Plugins.Core.IntegrationTest
+{
+    using System;
+    using Microsoft.Xrm.Sdk;
+
+    /// <summary>The crm cleaner.</summary>
+    public class CrmCleaner
+    {
+        private readonly IOrganizationService organizationService;
+
+        /// <summary>Initialises a new instance of the <see cref="CrmCleaner"/> class.</summary>
+        /// <param name="organizationService">The organization service.</param>
+        public CrmCleaner(IOrganizationService organizationService)
+        {
+            this.organizationService = organizationService;
+        }
+
+        /// <summary>The delete entity.</summary>
+        /// <param name="entityName">The entity name.</param>
+        /// <param name="entityId">The entity id.</param>
+        public void DeleteEntity(string entityName, Guid entityId)
+        {
+            organizationService.Delete(entityName, entityId);
+        }
+    }
+}
