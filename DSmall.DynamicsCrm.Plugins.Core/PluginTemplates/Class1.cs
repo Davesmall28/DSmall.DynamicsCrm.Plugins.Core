b79ce8f735ef723e27867208a6cb9ea48df37afa
Davesmall28 <dave@dsmall.co.uk>
2015-04-07 09:57:34 +0100

    Changed name of folder PluginTemplate to PluginTemplates.

---
+++ DSmall.DynamicsCrm.Plugins.Core/PluginTemplates/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace DSmall.DynamicsCrm.Plugins.Core
+{
+    using Microsoft.Xrm.Sdk;
+
+    public abstract class NewPlugin : Plugin
+    {
+        /// <summary>The execute.</summary>
+        /// <param name="organizationService">The organization service.</param>
+        /// <param name="pluginExecutionContext">The plugin execution context.</param>
+        /// <param name="tracingService">The tracing service.</param>
+        public override void Execute(
+            IOrganizationService organizationService,
+            IPluginExecutionContext pluginExecutionContext,
+            ITracingService tracingService)
+        {
+            var target = pluginExecutionContext.InputParameters.GetParameter<Entity>(InputParameterType.Target);
+
+            Execute(organizationService, pluginExecutionContext, tracingService, target);
+        }
+        
+        public abstract void Execute(
+            IOrganizationService organizationService,
+            IPluginExecutionContext pluginExecutionContext,
+            ITracingService tracingService,
+            Entity target);
+    }
+}
