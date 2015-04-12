41ff1242630313f532dcdce2fcbde164d6d10459
Davesmall28 <dave@dsmall.co.uk>
2015-04-12 10:52:31 +0100

    Added unit test for assign plugin.

---
+++ DSmall.DynamicsCrm.Plugins.Core.UnitTest/AssignPlugin/AssignSpecificationFixture.cs
@@ -0,0 +1,24 @@
+﻿namespace DSmall.DynamicsCrm.Plugins.Core.UnitTest
+{
+    using System;
+    using Microsoft.Xrm.Sdk;
+
+    /// <summary>The add to queue specification fixture.</summary>
+    public class AssignSpecificationFixture : SpecificationFixture<DummyAssignPlugin>
+    {
+        /// <summary>The perform test setup.</summary>
+        public override void PerformTestSetup()
+        {
+            ServiceProvider = new ServiceProviderInitializer().Setup().WithInputParameters(GetDummyInputParameters());
+        }
+
+        private ParameterCollection GetDummyInputParameters()
+        {
+            return new ParameterCollection
+            {
+                { "Target", new EntityReference("letter", Guid.NewGuid()) },
+                { "Assignee", new EntityReference("systemuser", Guid.NewGuid()) }
+            };
+        }
+    }
+}
