ca074f03e122d72d20e7602d9ce70f75aaccf608
Davesmall28 <dave@dsmall.co.uk>
2015-04-23 08:04:49 +0100

    Added close quote plugin integration tests.

---
+++ DSmall.DynamicsCrm.Plugins.Core.IntegrationTest/DSmall.DynamicsCrm.Plugins.Core.IntegrationTest/PluginTest/CloseQuote/CloseQuoteSpecification.cs
@@ -0,0 +1,63 @@
+namespace DSmall.DynamicsCrm.Plugins.Core.IntegrationTest
+{
+    using DSmall.UnitTest.Core;
+    using Microsoft.Xrm.Sdk;
+    using NUnit.Framework;
+
+    /// <summary>The close quote specification.</summary>
+    [TestFixture]
+    public class CloseQuoteSpecification : SpecificationBase
+    {
+        private CloseQuoteSpecificationFixture testFixture;
+
+        /// <summary>The should return input parameters containing two parameters.</summary>
+        [Test]
+        public void ShouldReturnInputParametersContainingTwoParameters()
+        {
+            Assert.IsTrue(testFixture.Result.InputParameters.Count == 2);
+        }
+
+        /// <summary>The should return input parameter containing quote close entity.</summary>
+        [Test]
+        public void ShouldReturnInputParameterContainingQuoteCloseEntity()
+        {
+            Assert.IsTrue(testFixture.Result.InputParameters.Count("QuoteClose", typeof(Entity)) == 1);
+        }
+
+        /// <summary>The should return input parameter containing status code.</summary>
+        [Test]
+        public void ShouldReturnInputParameterContainingStatusCode()
+        {
+            Assert.IsTrue(testFixture.Result.InputParameters.Count("Status", typeof(OptionSetValue)) == 1);
+        }
+
+        /// <summary>The should return no pre entity images.</summary>
+        [Test]
+        public void ShouldReturnNoPreEntityImages()
+        {
+            Assert.IsTrue(testFixture.Result.PreEntityImages.Count == 0);
+        }
+
+        /// <summary>The should return no post entity images.</summary>
+        [Test]
+        public void ShouldReturnNoPostEntityImages()
+        {
+            Assert.IsTrue(testFixture.Result.PostEntityImages.Count == 0);
+        }
+
+        /// <summary>The because of.</summary>
+        protected override void BecauseOf()
+        {
+            testFixture.CrmWriter.Execute(testFixture.RequestId, testFixture.CloseQuoteRequest);
+
+            testFixture.Result = Retry.Do(() => testFixture.EntitySerializer.Deserialize(testFixture.RequestId, testFixture.MessageName));
+        }
+
+        /// <summary>The context.</summary>
+        protected override void Context()
+        {
+            testFixture = new CloseQuoteSpecificationFixture();
+            testFixture.PerformTestSetup();
+        }
+    }
+}
